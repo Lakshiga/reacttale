b98762dd569cf20fa2bcdd85ea03b07c28ab2ec7
Lakshiga <171004880+Lakshiga@users.noreply.github.com>
2024-11-24 10:30:40 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './componets/Header';
+import Section from './componets/Section';
+import Footer from './componets/Footer';
+
+function App(){
+  return(
+    <>
+    <Header/>
+    <Section/>
+    <Footer/>
+    </>
+  );
+}
+
+
+export default App;
